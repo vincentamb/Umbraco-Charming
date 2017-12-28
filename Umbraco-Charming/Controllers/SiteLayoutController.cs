4d458c3230474e465daaae9118c5033d5baee2b6
vincentamb <33020904+vincentamb@users.noreply.github.com>
2017-12-28 11:55:05 +0530

    Add project files.

---
+++ Umbraco-Charming/Controllers/SiteLayoutController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace Umbraco_Charming.Controllers
+{
+    public class SiteLayoutController : SurfaceController
+    {
+        private const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/SiteLayout/";
+
+        public ActionResult RenderSentence()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Sentence.cshtml");
+        }
+        public ActionResult RenderCharmingSentence()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_CharmingSentence.cshtml");
+        }
+    }
+}
